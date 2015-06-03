ef46d3f14f3a22682131f7ae202eebf61c7aaaa8
JustasB <justasb@users.noreply.github.com>
2015-06-02 19:14:42 -0700

    Progress on Add Models in curator

--- neuroml_curator/app/models/author_list.rb
+++ neuroml_curator/app/models/author_list.rb
@@ -1,5 +1,7 @@
 class AuthorList < ActiveRecord::Base
   attr_accessible  :AuthorList_ID
+  has_many  :author_list_associations
+  has_many :persons, :through => :author_list_associations, :foreign_key => "AuthorList_ID"
   self.primary_key = "AuthorList_ID"
 end
 
--- neuroml_curator/app/models/author_list_association.rb
+++ neuroml_curator/app/models/author_list_association.rb
@@ -1,4 +1,4 @@
 class AuthorListAssociation < ActiveRecord::Base
   attr_accessible :Comments, :AuthorList_ID, :Person_ID ,:is_translator
-  self.primary_key = "AuthorList_ID,Person_ID"
+  self.primary_key = "AuthorList_ID"
 end
