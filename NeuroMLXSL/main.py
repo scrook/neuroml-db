b657318f9ddaabfbbc4b81dac046824cef0fbc80
JustasB <justasb@users.noreply.github.com>
2015-07-23 16:15:38 -0700

    Handle NML2 files for XML->HTML views

--- NeuroMLXSL/main.py
+++ NeuroMLXSL/main.py
@@ -3,14 +3,33 @@ import sys
 
 def main():
 
+    htmlNML1 = tryNML1()
+    htmlNML2 = tryNML2()
+
+    if htmlNML1.find('table') > 0:
+        print(htmlNML1)
+
+    elif htmlNML2.find('table') > 0:
+        print(htmlNML2)
+
+    else:
+        print('Sorry, there is no HTML view available for this file. You may go back and view the raw XML file.')
+
+def tryNML1():
     xml = ET.parse(sys.argv[1])
     xslt = ET.parse("/var/www/NeuroMLXSL/XSL/NeuroML_Level3_v1.8.1_HTML.xsl")
+    #xslt = ET.parse("XSL/NeuroML_Level3_v1.8.1_HTML.xsl")
     transform = ET.XSLT(xslt)
     html = transform(xml)
-    print(html)
-    #print(ET.tostring(html, pretty_print=True))
+    return str(html)
 
-
+def tryNML2():
+    xml = ET.parse(sys.argv[1])
+    xslt = ET.parse("/var/www/NeuroMLXSL/XSL/NeuroML2_To_HTML.xsl")
+    #xslt = ET.parse("XSL/NeuroML2_To_HTML.xsl")
+    transform = ET.XSLT(xslt)
+    html = transform(xml)
+    return str(html)
 
 if __name__ == "__main__":
     main()
