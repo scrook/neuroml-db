d38bfca5f50879c77e7a4786250b1efca05baa6b
Justas Birgiolas <birgiolasj@yahoo.com>
2017-11-21 15:48:29 -0700

    Tables fix

--- www/NeuroML-DB.org_Ontology/Queries/GetNeuroMLmodels.sql
+++ www/NeuroML-DB.org_Ontology/Queries/GetNeuroMLmodels.sql
@@ -1,7 +1,7 @@
-SELECT mma.Model_ID, nlx.NeuroLex_URI, Model_Name
+SELECT mma.Model_ID, nlx.NeuroLex_URI, m.Name
 FROM neurolexes nlx
 JOIN model_metadata_associations mma ON nlx.NeuroLex_ID = mma.Metadata_ID
-INNER JOIN all_models_view a ON mma.Model_ID=a.Model_ID
+JOIN models m ON mma.Model_ID=m.Model_ID
 WHERE NeuroLex_URI IN
 (
   [NeuroLexUris]
