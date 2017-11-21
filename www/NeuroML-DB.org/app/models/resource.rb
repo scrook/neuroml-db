9e121dd212eddebf557833c2d5f43d262d0c078d
Justas Birgiolas <birgiolasj@yahoo.com>
2017-11-21 15:16:03 -0700

    SciCrunch link fix, keywords & neurolex moved up

--- www/NeuroML-DB.org/app/models/resource.rb
+++ www/NeuroML-DB.org/app/models/resource.rb
@@ -1,4 +1,4 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :Name, :LogoUrl, :HomePageUrl
+  attr_accessible :Name, :LogoUrl, :HomePageUrl, :SciCrunch_RRID
   self.primary_key = "Resource_ID"
 end
