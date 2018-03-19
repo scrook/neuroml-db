c80601d9f5c6c7cc4365ef315a7ed7a80f5e63a9
Justas Birgiolas <jbirgio@gmail.com>
2018-03-18 18:08:47 -0700

    Route file cleanup

--- www/NeuroML-DB.org/config/routes.rb
+++ www/NeuroML-DB.org/config/routes.rb
@@ -1,28 +1,32 @@
 RedmineApp::Application.routes.draw do
   root :to => 'welcome#search_model', :as => 'search_model'
-  match 'search_test', :to => 'welcome#search_test', :as => 'search_test'
   match 'home_db', :to => 'welcome#home_db', :as => 'home_db'
   match 'documentation', :to => 'welcome#documentation', :as => 'documentation'
-  match 'render_xml_file', :to => 'xml#render_xml_file', :as => 'render_xml_file'
-  match 'render_xml_as_html', :to => 'xml#render_xml_as_html', :as => 'render_xml_as_html'
-  match 'modeldb_model_list', :to => 'modeldb#model_list', :as => 'model_list'
   match 'tool_support', :to => 'welcome#tool_support', :as => 'tool_support'
-  match 'model_submit', :to => 'welcome#model_submit', :as => 'model_submit'
   match 'search_model', :to => 'welcome#search_model', :as => 'search_model'
   match 'search_python', :to => 'welcome#search_python', :as => 'search_python'
-  match 'search_process_test', :to => 'welcome#search_process_test', :as => 'search_process_test'
   match 'search_process', :to => 'welcome#search_process', :as => 'search_process'
   match 'model_info', :to => 'welcome#model_info', :as => 'model_info'
-  match 'GetModelProtocolData', :to => 'welcome#GetModelProtocolData', :as => 'GetModelProtocolData'
+
+  match 'model_submit', :to => 'welcome#model_submit', :as => 'model_submit'
   match 'submission', :to => 'welcome#submission', :as => 'submission'
   match 'submission_error' , :to => 'welcome#submission_error', :as => 'submission_error'
-  match 'performance_test', :to => 'testing#performance_test', :as => 'performance_test'
-  match 'GetModelZip', :to => 'model#GetModelZip', :as => 'GetModelZip'
-  match 'GetChannelLEMSZip', :to => 'model#GetChannelLEMSZip', :as => 'GetChannelLEMSZip'
 
-  match 'api', :to => 'welcome#api'
-  match 'api/models', :to => 'models#all'
-  match 'api/model', :to => 'models#detail'
+  match 'search_process_test', :to => 'welcome#search_process_test', :as => 'search_process_test'
+  match 'search_test', :to => 'welcome#search_test', :as => 'search_test'
+  match 'performance_test', :to => 'testing#performance_test', :as => 'performance_test'
+
+  match 'modeldb_model_list', :to => 'modeldb#model_list', :as => 'model_list'
+  match 'render_xml_file', :to => 'xml#render_xml_file', :as => 'render_xml_file'
+  match 'render_xml_as_html', :to => 'xml#render_xml_as_html', :as => 'render_xml_as_html'
+  match 'GetModelProtocolData', :to => 'welcome#GetModelProtocolData', :as => 'GetModelProtocolData'
+
+  match 'GetModelZip', :to => 'models#GetModelZip', :as => 'GetModelZip'
+  match 'GetChannelLEMSZip', :to => 'models#GetChannelLEMSZip', :as => 'GetChannelLEMSZip'
+
+  match '/api', :to => 'welcome#api'
+  match '/api/models', :to => 'models#all'
+  match '/api/model', :to => 'models#detail'
   match '/api/models_for_modeldb', :to => 'modeldb#model_list'
 
 end
