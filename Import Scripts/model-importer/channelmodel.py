1ce17fabd726714c4fb942f63df773224bcd661c
Justas Birgiolas <jbirgio@gmail.com>
2018-06-20 12:23:09 -0700

    channel protocol refactoring

--- Import Scripts/model-importer/channelmodel.py
+++ Import Scripts/model-importer/channelmodel.py
@@ -5,3 +5,8 @@ class ChannelModel(NMLDB_Model):
     def __init__(self, *args, **kwargs):
         super(ChannelModel, self).__init__(*args, **kwargs)
 
+        self.all_properties.extend([
+            'tolerances',
+            'stability_range',
+            'DT_SENSITIVITY',
+        ])
